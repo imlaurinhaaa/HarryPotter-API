4bc226b2c3113efe2ea6831f397dfa585ea406d9
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-04-08 10:37:21 -0300

    feat: create filter- getWizards

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -20,8 +20,24 @@ INSERT INTO houses (name, founder) VALUES
     ('Corvinal', 'Rowena Ravenclaw'),
     ('Lufa-Lufa', 'Helga Hufflepuff');
 
-INSERT INTO wizards (name, house_id) VALUES 
-    ('Harry Potter', 1),
-    ('Draco Malfoy', 2),
-    ('Luna Lovegood', 3),
-    ('Cedrico Diggory', 4);
+INSERT INTO wizards (name, house_id) VALUES
+    ('Hermione Granger', 1),
+    ('Ron Weasley', 1),
+    ('Neville Longbottom', 1),
+    ('Ginny Weasley', 1),
+    ('Severus Snape', 2),
+    ('Pansy Parkinson', 2),
+    ('Gregory Goyle', 2),
+    ('Vincent Crabbe', 2),
+    ('Cho Chang', 3),
+    ('Padma Patil', 3),
+    ('Terry Boot', 3),
+    ('Michael Corner', 3),
+    ('Nymphadora Tonks', 4),
+    ('Hannah Abbott', 4),
+    ('Susan Bones', 4),
+    ('Ernie Macmillan', 4),
+    ('Dean Thomas', 1),
+    ('Blaise Zabini', 2),
+    ('Anthony Goldstein', 3),
+    ('Justin Finch-Fletchley', 4);
